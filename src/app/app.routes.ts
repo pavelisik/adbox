2e4659c5ebe9e2487a425afd77a4657fd55a6339
Pavelisik <pavelisik@gmail.com>
2025-08-30 11:13:35 +0300

    main components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { MainLayout } from '@app/layouts/main-layout/main-layout';
+import { HomePage } from '@app/pages/home-page/home-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: MainLayout,
+        children: [{ path: '', component: HomePage }],
+    },
+];
